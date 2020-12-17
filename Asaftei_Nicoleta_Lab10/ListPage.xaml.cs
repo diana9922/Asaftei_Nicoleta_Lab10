0c9fdcf3aee0ec62a5660cf07022f0c978192e3b
diana asaftei <nicoleta.diana.22@gmail.com>
2020-12-17 17:34:12 +0200

    Lab11

--- Asaftei_Nicoleta_Lab10/ListPage.xaml.cs
+++ Asaftei_Nicoleta_Lab10/ListPage.xaml.cs
@@ -18,6 +18,14 @@ namespace Asaftei_Nicoleta_Lab10
             InitializeComponent();
         }
 
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
         async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             var slist = (ShopList)BindingContext;
@@ -33,7 +41,25 @@ namespace Asaftei_Nicoleta_Lab10
 
         }
 
+       /* async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
 
+        }*/
 
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
         }
+
+
+
+    }
 }
